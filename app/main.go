db992e8a2772539bff079d479d20d2bd3ede7a08
zulfkhar00 <olxzulfar@gmail.com>
2025-03-09 15:32:47 +0000

    ping init

--- app/main.go
+++ app/main.go
@@ -17,9 +17,10 @@ func main() {
 		fmt.Println("Failed to bind to port 6379")
 		os.Exit(1)
 	}
-	_, err = l.Accept()
+	connection, err := l.Accept()
 	if err != nil {
 		fmt.Println("Error accepting connection: ", err.Error())
 		os.Exit(1)
 	}
+	connection.Write(([]byte)("+PONG\r\n"))
 }
