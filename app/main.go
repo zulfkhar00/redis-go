6665b093c5c49f89f18e314a9c8a4827d1384b2c
zulfkhar00 <olxzulfar@gmail.com>
2025-03-09 22:14:49 +0000

    implement exipre time

--- app/main.go
+++ app/main.go
@@ -6,7 +6,9 @@ import (
 	"io"
 	"net"
 	"os"
+	"strconv"
 	"strings"
+	"time"
 )
 
 const (
@@ -16,11 +18,11 @@ const (
 
 var _ = net.Listen
 var _ = os.Exit
-var kvStore map[string]string
+var kvStore map[string][]string
 
 func main() {
 	fmt.Println("Logs from your program will appear here!")
-	kvStore = make(map[string]string)
+	kvStore = make(map[string][]string)
 
 	l, err := net.Listen("tcp", "0.0.0.0:6379")
 	if err != nil {
@@ -42,9 +44,8 @@ func main() {
 
 func handleConnection(connection net.Conn) (err error) {
 	defer connection.Close()
-	buf := make([]byte, 1024)
 	for {
-		buf = buf[:cap(buf)]
+		buf := make([]byte, 1024)
 		n, err := connection.Read(buf)
 		if errors.Is(err, io.EOF) {
 			break
@@ -52,7 +53,6 @@ func handleConnection(connection net.Conn) (err error) {
 
 		buf = buf[:n]
 
-		fmt.Printf("command: %s", buf)
 		cmd, err := parseCommand(buf)
 		if err != nil {
 			return errors.New("parse command")
@@ -72,13 +72,13 @@ func handleConnection(connection net.Conn) (err error) {
 			buf = appendSimpleString(buf, result)
 		case "get":
 			result := get(cmd)
-			buf = appendSimpleString(buf, result)
+			buf = []byte(formatBulkString(result))
 		default:
-			panic(cmd[0])
+			buf = appendSimpleString(buf, "ERR unknown command")
 		}
 
 		_, err = connection.Write(buf)
-		if err != err {
+		if err != nil {
 			return err
 		}
 	}
@@ -87,18 +87,59 @@ func handleConnection(connection net.Conn) (err error) {
 }
 
 func set(cmd []string) string {
-	if len(cmd) != 3 {
-		return "ERR wrong number of arguments for command"
+	if len(cmd) == 3 {
+		kvStore[cmd[1]] = []string{cmd[2], "-1"}
+		return "OK"
 	}
-	kvStore[cmd[1]] = cmd[2]
-	return "OK"
+
+	if len(cmd) == 5 && strings.ToLower(cmd[3]) == "px" {
+		// check that expiration time is number
+		milliseconds, err := strconv.Atoi(cmd[4])
+		if err != nil {
+			return "incorrect expiration time"
+		}
+
+		currentTime := time.Now()
+		expireTime := currentTime.Add(time.Duration(milliseconds) * time.Millisecond)
+
+		kvStore[cmd[1]] = []string{cmd[2], millisecondsSince1970(expireTime)}
+		return "OK"
+	}
+
+	return "ERR wrong number of arguments for command"
 }
 
 func get(cmd []string) string {
 	if len(cmd) != 2 {
 		return "ERR wrong number of arguments for command"
 	}
-	return kvStore[cmd[1]]
+	valObj, ok := kvStore[cmd[1]]
+	if !ok {
+		return ""
+	}
+	val, expireTime := valObj[0], valObj[1]
+	if expireTime == "-1" {
+		return val
+	}
+	if expireTime < millisecondsSince1970(time.Now()) {
+		return ""
+	}
+	return val
+}
+
+func formatBulkString(value string) string {
+	if value == "" {
+		// Return null bulk string format for non-existent or empty values
+		return "$-1\r\n"
+	}
+	// Return the bulk string format: $<length>\r\n<value>\r\n
+	return "$" + strconv.Itoa(len(value)) + "\r\n" + value + "\r\n"
+}
+
+func millisecondsSince1970(t time.Time) string {
+	seconds := t.Unix()
+	milliseconds := seconds*1000 + int64(t.Nanosecond())/1000000
+	return fmt.Sprint(milliseconds)
 }
 
 func appendSimpleString(buf []byte, str string) []byte {
